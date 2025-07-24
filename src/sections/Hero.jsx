e708ed1b65f7caec76b4781567778192858c671f
Atish <atishjadhav835@gmail.com>
2025-07-24 21:53:11 +0530

    added custom logo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,6 +20,7 @@ const Hero = () => {
         <div className="mt-6 flex gap-5 justify-center items-center md:justify-start">
           <a
             href="https://drive.google.com/file/d/1yCVrcGn6WU9IXIoWW3dlTupdNNqA7noF/view?usp=sharing"
+            //https://drive.google.com/file/d/1bHTY8GR8sdQnWN5Lw-kQ4Ey9hQeZKQ1w/view?usp=sharing
             target="blank"
             className="hover:bg-[#121932] py-2 px-5 rounded-md bg-[#ffffff] transition font-medium text-black border hover:text-white cursor-pointer"
           >
